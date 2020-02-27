31c6bf75992398c039542debe8d69632350d386d
aplefull <mraplefull@yandex.by>
2020-02-27 21:56:48 +0300

    add algorithmic tasks

---
+++ 2019/RSSchool/core-js/08-conditions-n-loops-tasks.js
@@ -0,0 +1,560 @@
+/* *************************************************************************************************
+ *                                                                                                *
+ * Plese read the following tutorial before implementing tasks:                                   *
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Control_flow_and_error_handling  *
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Loops_and_iteration              *
+ *                                                                                                *
+ ************************************************************************************************ */
+
+
+/**
+ * Returns the 'Fizz','Buzz' or an original number using the following rules:
+ * 1) return original number
+ * 2) but if number multiples of three return 'Fizz'
+ * 3) for the multiples of five return 'Buzz'
+ * 4) for numbers which are multiples of both three and five return 'FizzBuzz'
+ *
+ * @param {number} num
+ * @return {any}
+ *
+ * @example
+ *   2 =>  2
+ *   3 => 'Fizz'
+ *   5 => 'Buzz'
+ *   4 => 4
+ *  15 => 'FizzBuzz'
+ *  20 => 'Buzz'
+ *  21 => 'Fizz'
+ *
+ */
+function getFizzBuzz(num) {
+  let str = '';
+
+  if (num % 3 === 0) str += 'Fizz';
+  if (num % 5 === 0) str += 'Buzz';
+
+  else if (str === '') return num.toString();
+  return str;
+}
+
+
+/**
+ * Returns the factorial of the specified integer n.
+ *
+ * @param {number} n
+ * @return {number}
+ *
+ * @example:
+ *   1  => 1
+ *   5  => 120
+ *   10 => 3628800
+ */
+function getFactorial(n) {
+  if (n === 0) return 1;
+  return n * getFactorial(n - 1);
+}
+
+
+/**
+ * Returns the sum of integer numbers between n1 and n2 (inclusive).
+ *
+ * @param {number} n1
+ * @param {number} n2
+ * @return {number}
+ *
+ * @example:
+ *   1,2   =>  3  ( = 1+2 )
+ *   5,10  =>  45 ( = 5+6+7+8+9+10 )
+ *   -1,1  =>  0  ( = -1 + 0 + 1 )
+ */
+function getSumBetweenNumbers(n1, n2) {
+  let sum = 0;
+  for (let i = n1; i <= n2; i += 1) {
+    sum += i;
+  }
+
+  return sum;
+}
+
+
+/**
+ * Returns true, if a triangle can be built with the specified sides a, b, c
+ * and false in any other ways.
+ *
+ * @param {number} a
+ * @param {number} b
+ * @param {number} c
+ * @return {bool}
+ *
+ * @example:
+ *   1,2,3    =>  false
+ *   3,4,5    =>  true
+ *   10,1,1   =>  false
+ *   10,10,10 =>  true
+ */
+function isTriangle(a, b, c) {
+  if (a + b <= c || a + c <= b || b + c <= a) return false;
+  return true;
+}
+
+
+/**
+ * Returns true, if two specified axis-aligned rectangles overlap, otherwise false.
+ * Each rectangle representing by object
+ *  {
+ *     top: 5,
+ *     left: 5,
+ *     width: 20,
+ *     height: 10
+ *  }
+ *
+ *  (5;5)
+ *     -------------
+ *     |           |
+ *     |           |  height = 10
+ *     -------------
+ *        width=20
+ *
+ * NOTE: Please use canvas coordinate space (https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Drawing_shapes#The_grid),
+ * it differs from Cartesian coordinate system.
+ *
+ * @param {object} rect1
+ * @param {object} rect2
+ * @return {bool}
+ *
+ * @example:
+ *   { top: 0, left: 0, width: 10, height: 10 },
+ *   { top: 5, left: 5, width: 20, height: 20 }    =>  true
+ *
+ *   { top: 0, left: 0, width: 10, height: 10 },
+ *   { top:20, left:20, width: 20, height: 20 }    =>  false
+ *
+ */
+function doRectanglesOverlap(rect1, rect2) {
+  if (
+    (((rect1.left + rect1.width) > rect2.left) || ((rect2.left + rect2.width) < rect1.left))
+    && (((rect1.top + rect1.height) > rect2.top) || ((rect2.top + rect2.height) < rect1.top))
+  ) return true;
+
+  return false;
+}
+
+
+/**
+ * Returns true, if point lies inside the circle, otherwise false.
+ * Circle is an object of
+ *  {
+ *     center: {
+ *       x: 5,
+ *       y: 5
+ *     },
+ *     radius: 20
+ *  }
+ *
+ * Point is object of
+ *  {
+ *     x: 5,
+ *     y: 5
+ *  }
+ *
+ * @param {object} circle
+ * @param {object} point
+ * @return {bool}
+ *
+ * @example:
+ *   { center: { x:0, y:0 }, radius:10 },  { x:0, y:0 }     => true
+ *   { center: { x:0, y:0 }, radius:10 },  { x:10, y:10 }   => false
+ *
+ */
+function isInsideCircle(circle, point) {
+  if (Math.hypot(point.x - circle.center.x, point.y - circle.center.y) < circle.radius) return true;
+  return false;
+}
+
+
+/**
+ * Returns the first non repeated char in the specified strings otherwise returns null.
+ *
+ * @param {string} str
+ * @return {string}
+ *
+ * @example:
+ *   'The quick brown fox jumps over the lazy dog' => 'T'
+ *   'abracadabra'  => 'c'
+ *   'entente' => null
+ */
+function findFirstSingleChar(str) {
+  for (let i = 0; i < str.length; i += 1) {
+    const char = str[i];
+    if (str.indexOf(char, i + 1) === -1 && str.indexOf(char) === i) {
+      return char;
+    }
+  }
+  return null;
+}
+
+
+/**
+ * Returns the string representation of math interval,
+ * specified by two points and include / exclude flags.
+ * See the details: https://en.wikipedia.org/wiki/Interval_(mathematics)
+ *
+ * Please take attention, that the smaller number should be the first in the notation
+ *
+ * @param {number} a
+ * @param {number} b
+ * @param {bool} isStartIncluded
+ * @param {bool} isEndIncluded
+ * @return {string}
+ *
+ * @example
+ *   0, 1, true, true   => '[0, 1]'
+ *   0, 1, true, false  => '[0, 1)'
+ *   0, 1, false, true  => '(0, 1]'
+ *   0, 1, false, false => '(0, 1)'
+ * Smaller number has to be first :
+ *   5, 3, true, true   => '[3, 5]'
+ *
+ */
+function getIntervalString(a, b, isStartIncluded, isEndIncluded) {
+  let str;
+
+  if (a > b) str = `${b}, ${a}`;
+  else str = `${a}, ${b}`;
+
+  if (isStartIncluded) str = `[${str}`;
+  else str = `(${str}`;
+
+  if (isEndIncluded) str += ']';
+  else str += ')';
+
+  return str;
+}
+
+
+/**
+ * Reverse the specified string (put all chars in reverse order)
+ *
+ * @param {string} str
+ * @return {string}
+ *
+ * @example:
+ * 'The quick brown fox jumps over the lazy dog' => 'god yzal eht revo spmuj xof nworb kciuq ehT'
+ * 'abracadabra' => 'arbadacarba'
+ * 'rotator' => 'rotator'
+ * 'noon' => 'noon'
+ */
+function reverseString(str) {
+  return str.split('').reverse().join('');
+}
+
+
+/**
+ * Reverse the specified integer number (put all digits in reverse order)
+ *
+ * @param {number} num
+ * @return {number}
+ *
+ * @example:
+ *   12345 => 54321
+ *   1111  => 1111
+ *   87354 => 45378
+ *   34143 => 34143
+ */
+function reverseInteger(num) {
+  return parseInt(num.toString().split('').reverse().join(''), 10);
+}
+
+
+/**
+ * Validates the CCN (credit card number) and return true if CCN is valid
+ * and false otherwise.
+ *
+ * See algorithm here : https://en.wikipedia.org/wiki/Luhn_algorithm
+ *
+ * @param {number} cnn
+ * @return {boolean}
+ *
+ * @example:
+ *   79927398713      => true
+ *   4012888888881881 => true
+ *   5123456789012346 => true
+ *   378282246310005  => true
+ *   371449635398431  => true
+ *
+ *   4571234567890111 => false
+ *   5436468789016589 => false
+ *   4916123456789012 => false
+ */
+function isCreditCardNumber(ccn) {
+  const arr = ccn.toString().split('');
+  if (arr.length % 2 === 0) {
+    for (let i = 0; i < arr.length; i += 2) {
+      arr[i] = (arr[i] * 2).toString();
+      if (arr[i].length > 1) arr[i] = +arr[i][0] + +arr[i][1];
+    }
+  } else {
+    for (let i = 1; i < arr.length; i += 2) {
+      arr[i] = (arr[i] * 2).toString();
+      if (arr[i].length > 1) arr[i] = +arr[i][0] + +arr[i][1];
+    }
+  }
+
+  let val = 0;
+  const res = arr.reduce((acc, v) => { const vNum = +v; val += vNum; return val; }, 0).toString();
+  // res = val.toString();
+
+  if (res[res.length - 1] === '0') return true;
+  return false;
+}
+
+/**
+ * Returns the digital root of integer:
+ *   step1 : find sum of all digits
+ *   step2 : if sum > 9 then goto step1 otherwise return the sum
+ *
+ * @param {number} n
+ * @return {number}
+ *
+ * @example:
+ *   12345 ( 1+2+3+4+5 = 15, 1+5 = 6) => 6
+ *   23456 ( 2+3+4+5+6 = 20, 2+0 = 2) => 2
+ *   10000 ( 1+0+0+0+0 = 1 ) => 1
+ *   165536 (1+6+5+5+3+6 = 26,  2+6 = 8) => 8
+ */
+function getDigitalRoot(num) {
+  return ((num - 1) % 9) + 1;
+}
+
+
+/**
+ * Returns true if the specified string has the balanced brackets and false otherwise.
+ * Balanced means that is, whether it consists entirely of pairs of opening/closing brackets
+ * (in that order), none of which mis-nest.
+ * Brackets include [],(),{},<>
+ *
+ * @param {string} str
+ * @return {boolean}
+ *
+ * @example:
+ *   '' => true
+ *   '[]'  => true
+ *   '{}'  => true
+ *   '()   => true
+ *   '[[]' => false
+ *   ']['  => false
+ *   '[[][][[]]]' => true
+ *   '[[][]][' => false
+ *   '{)' = false
+ *   '{[(<{[]}>)]}' = true
+ */
+function isBracketsBalanced(str) {
+  let matchingOpeningBracket;
+  let char;
+  const stack = [];
+
+  const openingBrackets = ['[', '{', '(', '<'];
+  const closingBrackets = [']', '}', ')', '>'];
+
+  for (let i = 0; i < str.length; i += 1) {
+    char = str[i];
+
+    if (closingBrackets.indexOf(char) !== -1) {
+      matchingOpeningBracket = openingBrackets[closingBrackets.indexOf(char)];
+      if (stack.length === 0 || (stack.pop() !== matchingOpeningBracket)) {
+        return false;
+      }
+    } else {
+      stack.push(char);
+    }
+  }
+
+  return (stack.length === 0);
+}
+
+
+/**
+ * Returns the string with n-ary (binary, ternary, etc, where n <= 10)
+ * representation of specified number.
+ * See more about
+ * https://en.wikipedia.org/wiki/Binary_number
+ * https://en.wikipedia.org/wiki/Ternary_numeral_system
+ * https://en.wikipedia.org/wiki/Radix
+ *
+ * @param {number} num
+ * @param {number} n, radix of the result
+ * @return {string}
+ *
+ * @example:
+ *   1024, 2  => '10000000000'
+ *   6561, 3  => '100000000'
+ *    365, 2  => '101101101'
+ *    365, 3  => '111112'
+ *    365, 4  => '11231'
+ *    365, 10 => '365'
+ */
+function toNaryString(num, n) {
+  return num.toString(n);
+}
+
+
+/**
+ * Returns the commom directory path for specified array of full filenames.
+ *
+ * @param {array} pathes
+ * @return {string}
+ *
+ * @example:
+ *   ['/web/images/image1.png', '/web/images/image2.png']  => '/web/images/'
+ *   ['/web/assets/style.css', '/web/scripts/app.js',  'home/setting.conf'] => ''
+ *   ['/web/assets/style.css', '/.bin/mocha',  '/read.me'] => '/'
+ *   ['/web/favicon.ico', '/web-scripts/dump', '/webalizer/logs'] => '/'
+ */
+function getCommonDirectoryPath(pathes) {
+  const arr = [];
+  let str = '';
+  let done = false;
+
+  for (let i = 0; i < pathes.length; i += 1) {
+    arr.push(pathes[i].split('/'));
+  }
+
+  for (let i = 0; i < pathes.length - 1; i += 1) {
+    if (pathes[i][0] !== pathes[i + 1][0]) return '';
+  }
+
+  for (let j = 0; j < arr[0].length; j += 1) {
+    for (let i = 0; i < arr.length - 1; i += 1) {
+      if (arr[i][j] !== arr[i + 1][j]) {
+        done = true;
+        break;
+      }
+
+      if (i === arr.length - 2) {
+        str += `${arr[i][j]}/`;
+      }
+    }
+
+    if (done) break;
+  }
+
+  return str;
+}
+
+
+/**
+ * Returns the product of two specified matrixes.
+ * See details: https://en.wikipedia.org/wiki/Matrix_multiplication
+ *
+ * @param {array} m1
+ * @param {array} m2
+ * @return {array}
+ *
+ * @example:
+ *   [[ 1, 0, 0 ],       [[ 1, 2, 3 ],           [[ 1, 2, 3 ],
+ *    [ 0, 1, 0 ],   X    [ 4, 5, 6 ],     =>     [ 4, 5, 6 ],
+ *    [ 0, 0, 1 ]]        [ 7, 8, 9 ]]            [ 7, 8, 9 ]]
+ *
+ *                        [[ 4 ],
+ *   [[ 1, 2, 3]]    X     [ 5 ],          =>     [[ 32 ]]
+ *                         [ 6 ]]
+ *
+ */
+function getMatrixProduct(a, b) {
+  const matrix = new Array(a.length);
+  let sum = 0;
+  let k = -1;
+
+  for (let i = 0; i < matrix.length; i += 1) {
+    matrix[i] = new Array(b[0].length);
+  }
+
+  for (let i = 0; i < a.length; i += 1) {
+    for (let n = 0; n < b[0].length; n += 1) {
+      for (let j = 0; j < a[0].length; j += 1) {
+        sum += a[i][j] * b[j][k + 1];
+      }
+      k += 1;
+      matrix[i][k] = sum;
+      sum = 0;
+    }
+    k = -1;
+  }
+  return matrix;
+}
+
+
+/**
+ * Returns the evaluation of the specified tic-tac-toe position.
+ * See the details: https://en.wikipedia.org/wiki/Tic-tac-toe
+ *
+ * Position is provides as 3x3 array with the following values: 'X','0', undefined
+ * Function should return who is winner in the current position according to the game rules.
+ * The result can be: 'X','0',undefined
+ *
+ * @param {array} position
+ * @return {string}
+ *
+ * @example
+ *
+ *   [[ 'X',   ,'0' ],
+ *    [    ,'X','0' ],       =>  'X'
+ *    [    ,   ,'X' ]]
+ *
+ *   [[ '0','0','0' ],
+ *    [    ,'X',    ],       =>  '0'
+ *    [ 'X',   ,'X' ]]
+ *
+ *   [[ '0','X','0' ],
+ *    [    ,'X',    ],       =>  undefined
+ *    [ 'X','0','X' ]]
+ *
+ *   [[    ,   ,    ],
+ *    [    ,   ,    ],       =>  undefined
+ *    [    ,   ,    ]]
+ *
+ */
+function evaluateTicTacToePosition(field) {
+  for (let i = 0; i < 3; i += 1) {
+    if (field[i][0] === field[i][1] && field[i][1] === field[i][2] && field[i][2] !== undefined) {
+      return field[i][2];
+    }
+  }
+
+  for (let i = 0; i < 3; i += 1) {
+    if (field[0][i] === field[1][i] && field[1][i] === field[2][i] && field[2][i] !== undefined) {
+      return field[2][i];
+    }
+  }
+
+  if (field[0][0] === field[1][1] && field[1][1] === field[2][2] && field[2][2] !== undefined) {
+    return field[2][2];
+  }
+
+  if (field[0][2] === field[1][1] && field[1][1] === field[2][0] && field[2][0] !== undefined) {
+    return field[2][0];
+  }
+
+  return undefined;
+}
+
+
+module.exports = {
+  getFizzBuzz,
+  getFactorial,
+  getSumBetweenNumbers,
+  isTriangle,
+  doRectanglesOverlap,
+  isInsideCircle,
+  findFirstSingleChar,
+  getIntervalString,
+  reverseString,
+  reverseInteger,
+  isCreditCardNumber,
+  getDigitalRoot,
+  isBracketsBalanced,
+  toNaryString,
+  getCommonDirectoryPath,
+  getMatrixProduct,
+  evaluateTicTacToePosition,
+};
---
+++ 2019/RSSchool/core-js/09-functions-n-closures-tasks.js
@@ -0,0 +1,213 @@
+/* *********************************************************************************************
+ *                                                                                             *
+ * Plese read the following tutorial before implementing tasks:                                *
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Functions                     *
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function   *
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/arguments       *
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Closures                            *
+ *                                                                                             *
+ ********************************************************************************************* */
+
+
+/**
+ * Returns the functions composition of two specified functions f(x) and g(x).
+ * The result of compose is to be a function of one argument, (lets call the argument x),
+ * which works like applying function f to the result of applying function g to x, i.e.
+ *  getComposition(f,g)(x) = f(g(x))
+ *
+ * @param {Function} f
+ * @param {Function} g
+ * @return {Function}
+ *
+ * @example
+ *   getComposition(Math.sin, Math.asin)(x) => Math.sin(Math.asin(x))
+ *
+ */
+function getComposition(f, g) {
+  return (...args) => f(g(...args));
+}
+
+
+/**
+ * Returns the math power function with the specified exponent
+ *
+ * @param {number} exponent
+ * @return {Function}
+ *
+ * @example
+ *   const power2 = getPowerFunction(2); // => x^2
+ *   power2(2) => 4
+ *   power2(4) => 16
+ *
+ *   const power05 = getPowerFunction(0.5); // => x^0.5
+ *   power05(4) => 2
+ *   power05(16) => 4
+ *
+ */
+function getPowerFunction(exponent) {
+  return (a) => a ** exponent;
+}
+
+
+/**
+ * Returns the polynom function of one argument based on specified coefficients.
+ * See: https://en.wikipedia.org/wiki/Polynomial#Definition
+ *
+ * @params {integer}
+ * @return {Function}
+ *
+ * @example
+ *   getPolynom(2,3,5) => y = 2*x^2 + 3*x + 5
+ *   getPolynom(1,-3)  => y = x - 3
+ *   getPolynom(8)     => y = 8
+ *   getPolynom()      => null
+ */
+function getPolynom(...args) {
+  return (x) => Array.from(args).reverse().reduce((acc, val, i) => acc + val * x ** i);
+}
+
+
+/**
+ * Memoizes passed function and returns function
+ * which invoked first time calls the passed function and then always returns cached result.
+ *
+ * @params {Function} func - function to memoize
+ * @return {Function} memoized function
+ *
+ * @example
+ *   const memoizer = memoize(() => Math.random());
+ *   memoizer() => some random number  (first run, evaluates the result of Math.random())
+ *   memoizer() => the same random number  (second run, returns the previous cached result)
+ *   ...
+ *   memoizer() => the same random number  (next run, returns the previous cached result)
+ */
+function memoize(func) {
+  return (...args) => {
+    let key = JSON.stringify(args);
+    if (arguments.length === 0) key = 'void';
+
+    const temp = func;
+    temp.buff = temp.buff || {};
+
+    if (Object.prototype.hasOwnProperty.call(temp.buff, key)) return temp.buff[key];
+    temp.buff[key] = temp(...args);
+    return temp.buff[key];
+  };
+}
+
+
+/**
+ * Returns the function trying to call the passed function and if it throws,
+ * retrying it specified number of attempts.
+ *
+ * @param {Function} func
+ * @param {number} attempts
+ * @return {Function}
+ *
+ * @example
+ * const attempt = 0, retryer = retry(() => {
+ *      if (++attempt % 2) throw new Error('test');
+ *      else return attempt;
+ * }, 2);
+ * retryer() => 2
+ */
+function retry(func, attempts) {
+  return () => {
+    for (let i = 0; i <= attempts; i += 1) {
+      try {
+        return func();
+      } catch (e) { /* empty */ }
+    }
+    return true;
+  };
+}
+
+
+/**
+ * Returns the logging wrapper for the specified method,
+ * Logger has to log the start and end of calling the specified function.
+ * Logger has to log the arguments of invoked function.
+ * The fromat of output log is:
+ * <function name>(<arg1>, <arg2>,...,<argN>) starts
+ * <function name>(<arg1>, <arg2>,...,<argN>) ends
+ *
+ *
+ * @param {Function} func
+ * @param {Function} logFunc - function to output log with single string argument
+ * @return {Function}
+ *
+ * @example
+ *
+ * const cosLogger = logger(Math.cos, console.log);
+ * const result = cosLogger(Math.PI));     // -1
+ *
+ * log from console.log:
+ * cos(3.141592653589793) starts
+ * cos(3.141592653589793) ends
+ *
+ */
+function logger(func, logFunc) {
+  return (...args) => {
+    const jsonArgs = JSON.stringify(args).slice(1, -1);
+    logFunc(`${func.name}(${jsonArgs}) starts`);
+    const result = func(...args);
+    logFunc(`${func.name}(${jsonArgs}) ends`);
+    return result;
+  };
+}
+
+
+/**
+ * Return the function with partial applied arguments
+ *
+ * @param {Function} fn
+ * @return {Function}
+ *
+ * @example
+ *   const fn = function(x1,x2,x3,x4) { return  x1 + x2 + x3 + x4; };
+ *   partialUsingArguments(fn, 'a')('b','c','d') => 'abcd'
+ *   partialUsingArguments(fn, 'a','b')('c','d') => 'abcd'
+ *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
+ *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
+ */
+function partialUsingArguments(fn, ...first) {
+  return (...second) => fn(...first, ...second);
+}
+
+
+/**
+ * Returns the id generator function that returns next integer starting
+ * from specified number every time when invoking.
+ *
+ * @param {Number} startFrom
+ * @return {Function}
+ *
+ * @example
+ *   const getId4 = getIdGenerator(4);
+ *   const getId10 = gerIdGenerator(10);
+ *   getId4() => 4
+ *   getId10() => 10
+ *   getId4() => 5
+ *   getId4() => 6
+ *   getId4() => 7
+ *   getId10() => 11
+ */
+function getIdGeneratorFunction(startFrom) {
+  let i = startFrom - 1;
+  return () => {
+    i += 1;
+    return i;
+  };
+}
+
+
+module.exports = {
+  getComposition,
+  getPowerFunction,
+  getPolynom,
+  memoize,
+  retry,
+  logger,
+  partialUsingArguments,
+  getIdGeneratorFunction,
+};
